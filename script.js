8f251b07ca843587127383e51901180e228734a5
A S U S <sawungwulung7@gmail.com>
2023-06-21 17:59:49 +0800

    added

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#myForm').submit(function(e) {
+      e.preventDefault(); // Mencegah pengiriman formulir
+  
+      var formData = $(this).serialize(); // Mengambil data formulir
+  
+      $.ajax({
+        url: 'https://script.google.com/macros/s/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exe"https://script.google.com/macros/s/AKfycbyekmo2SdMHusXaYmKFuuRQrP7zEC0Hk-yA6NIDKTCJ5Q5ktItba_8IcD3udgFOO83i/exec',
+        method: 'POST',
+        data: formData,
+        success: function(response) {
+          console.log('Formulir berhasil dikirim');
+          // Handle respons sukses jika diperlukan
+  
+          // Menghapus nilai-nilai dalam elemen formulir
+          $('#myForm')[0].reset();
+        },
+        error: function(xhr, status, error) {
+          console.error('Error mengirim formulir:', error);
+          // Handle respons error jika diperlukan
+        }
+      });
+    });
+  });
+  
